a392ef95702b5d64178d0021d47603f8d55b556d
Eric Holt <fatmoti@gmail.com>
2018-12-30 10:52:01 -0500

    appendPageLinks initial functionally added

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 /*** 
    Add your global variables that store the DOM elements you will 
@@ -16,9 +15,8 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
-
-
+const mainPage = document.querySelector(".page");
+const studentList = document.getElementsByTagName("li");
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -34,17 +32,44 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
+function showPage() {
+  for (let i = 0; i < studentList.length; i++) {
+    studentList[i].style.display = "none";
+  }
+}
 
+/* 
+   Function to append pagination links to bottom of main page div.
+*/
+function appendPageLinks() {
+  console.log("appendPageLinks() was called.");
+  if (document.querySelector(".pagination") !== null) {
+    mainPage.removeChild(document.querySelector(".pagination"));
+    console.log("Removed pagination.");
+  }
+  const div = document.createElement("div");
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+  let count = 0;
 
+  for (let i = 0; i < studentList.length; i += 10) {
+    const a = document.createElement("a");
+    count++;
+    a.href = "#";
+    if (count === 1) {
+      a.className = "active";
+    }
+    a.textContent = count;
+    li.appendChild(a);
+    ul.appendChild(li);
 
+    // studentList[i].style.display = "none";
+  }
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
+  div.appendChild(ul);
+  div.className = "pagination";
+  mainPage.appendChild(div);
+}
+appendPageLinks();
+appendPageLinks();
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
