9182dc7de878363f3278a344c842aae81657a683
Eric Holt <fatmoti@gmail.com>
2019-01-01 19:01:44 -0500

    Added search functionality, refactored code, and added comments.

--- js/script.js
+++ js/script.js
@@ -3,68 +3,31 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const mainPage = document.querySelector(".page");
-const pageHeader = mainPage.firstElementChild;
-const studentName = document.getElementsByTagName("h3");
-const students = document.getElementsByClassName("student-item");
+const students = document.querySelectorAll(".student-item"); // This is used to build the array below, hide all students in showPage function, and in search function to build filtered studentList.
+let studentList = Array.apply(null, students); //Initially load studentList array this array will be changed as searched field is used
 
-/*
-   Temporary for loop to number students to confirm pagination is
-   0-9,10-19,20-29 etc.
-*/
-for (let i = 0; i < studentName.length; i++) {
-  const studentNumber = i + 1;
-  const currentStudent = studentName[i].textContent;
-
-  studentName[i].textContent = studentNumber + ") " + currentStudent;
-}
 /*
    This function accepts the current active page and shows only students in that range
 */
-function showPage(students, activePage) {
-  let startingIndex = activePage * 10 - 10;
-  let endingIndex = activePage * 10;
+function showPage(list, activePage) {
+  let visableListStop = activePage * 10;
+  let visableListStart = visableListStop - 10;
 
-  if (activePage === 1) {
-    // unhide active list
-    for (let i = 0; i < endingIndex; i++) {
-      students[i].style.display = "";
-    }
-    // hide rest of students
-    for (let i = endingIndex; i < students.length; i++) {
-      students[i].style.display = "none";
+  // Hide all students
+  for (let i = 0; i < students.length; i++) {
+    students[i].style.display = "none";
+  }
+  // Show all students in list if 10 or less
+  if (list.length < 11) {
+    for (let i = 0; i < list.length; i++) {
+      list[i].style.display = "";
     }
   } else {
-    startingIndex = activePage * 10 - 10;
-    endingIndex = activePage * 10;
-    if (endingIndex > students.length - 1) {
-      for (let i = 0; i < startingIndex; i++) {
-        students[i].style.display = "none";
-      }
-      for (let i = startingIndex; i < students.length; i++) {
-        students[i].style.display = "";
-      }
-    } else {
-      for (let i = startingIndex; i < endingIndex; i++) {
-        students[i].style.display = "";
-      }
-      for (let i = 0; i < startingIndex; i++) {
-        students[i].style.display = "none";
-      }
-      for (let i = endingIndex; i < students.length; i++) {
-        students[i].style.display = "none";
+    // Show ten students in list based on active page
+    for (let i = 0; i < list.length; i++) {
+      if (i >= visableListStart && i < visableListStop) {
+        list[i].style.display = "";
       }
     }
   }
@@ -72,42 +35,71 @@ function showPage(students, activePage) {
 
 /* 
    Function to append pagination links to bottom of main page div.
+   Parameters require an array of elements and the current active page.
 */
-function appendPageLinks(students) {
-  if (students.length < 10) {
-    return;
-  }
+function appendPageLinks(list, activePage) {
+  // Check to see if div exist and remove it if does.
   if (document.querySelector(".pagination") !== null) {
-    // confirm we are removing the existing pagination before adding new div
     mainPage.removeChild(document.querySelector(".pagination"));
   }
+  if (activePage === undefined || activePage === null) {
+    activePage = 1;
+  }
+  //If list is 10 or below then return without adding pagination
+  if (list.length < 11) {
+    return 1;
+  }
+
+  const numberOfPages = Math.ceil(list.length / 10);
   const div = document.createElement("div");
+  div.className = "pagination";
   const ul = document.createElement("ul");
   const li = document.createElement("li");
-  let count = 0;
-  for (let i = 0; i < students.length; i += 10) {
+
+  for (let i = 1; i <= numberOfPages; i++) {
     const a = document.createElement("a");
-    count++;
 
     a.href = "#";
-    if (count === 1) {
+    if (i === activePage) {
       a.className = "active";
     }
-    a.textContent = count;
+    a.textContent = i;
     li.appendChild(a);
     ul.appendChild(li);
   }
 
   div.appendChild(ul);
-  div.className = "pagination";
   mainPage.appendChild(div);
-  showPage(students, Number(document.querySelector(".active").textContent));
+
+  return activePage;
 }
 
+showPage(studentList, appendPageLinks(studentList, 1)); // Build our initial page with pagination links.
+
+const paginationDiv = document.querySelector(".pagination"); // Declared after pagination div has been inserted
+
+mainPage.addEventListener("click", e => {
+  e.preventDefault();
+
+  if (e.target.tagName === "A") {
+    const links = document.getElementsByTagName("a");
+    for (let i = 0; i < links.length; i++) {
+      links[i].className = "";
+    }
+    e.target.className = "active";
+
+    showPage(studentList, Number(e.target.textContent));
+  }
+});
+
 /*
-   Function to append search html to page.
+   Functionality for searching students.
+   First we add the search field to the page.
+   Event listeners listen for search button click and keyup events.
+   When triggered we call the search function. It will call the showPage functions and appendLinks function
 */
 function appendSearchField() {
+  const pageHeader = mainPage.firstElementChild;
   const div = document.createElement("div");
   const input = document.createElement("input");
   const button = document.createElement("button");
@@ -118,22 +110,34 @@ function appendSearchField() {
   div.appendChild(button);
   pageHeader.appendChild(div);
 }
-appendSearchField();
-appendPageLinks(students);
-const paginationDiv = document.querySelector(".pagination");
-paginationDiv.addEventListener("click", e => {
-  e.preventDefault();
-  const links = document.getElementsByTagName("a");
-  for (let i = 0; i < links.length; i++) {
-    links[i].className = "";
+appendSearchField(); // Add search field and button to page dynamically
+
+const searchDiv = document.querySelector(".student-search"); // Declared after search div has been inserted
+
+searchDiv.addEventListener("click", e => {
+  if (e.target.tagName === "BUTTON") {
+    search();
   }
-  e.target.className = "active";
-  showPage(students, Number(e.target.textContent));
 });
 
-mainPage.addEventListener("click", e => {
-  if (e.target.tagName === "BUTTON") {
-    const input = document.querySelector("input");
-    console.log(input.value);
-  }
+searchDiv.addEventListener("keyup", e => {
+  search();
 });
+
+const search = () => {
+  const input = document.querySelector("input");
+  const search = input.value.toUpperCase();
+  studentList.length = 0;
+
+  const h3 = document.querySelectorAll(".student-item h3");
+
+  for (let i = 0; i < students.length; i++) {
+    let textContent = h3[i].textContent.toUpperCase();
+
+    if (textContent.includes(search)) {
+      studentList.push(students[i]);
+    }
+  }
+
+  showPage(studentList, appendPageLinks(studentList, 1));
+};
